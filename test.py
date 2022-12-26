353b4c6cd53b3f69ff4266708298d8400247daa4
Lewis Cole <lcole.mail@gmail.com>
2022-12-26 19:53:54 +0000

    Add signpost text to test.py output

--- test.py
+++ test.py
@@ -6,16 +6,22 @@ from pokeranalysissuite.hand import parsecard, Hand
 
 # test if function parsecard in hand.py is giving desired outputs
 print(
-    parsecard("7h") == [5, 2]
-    and parsecard("Ac") == [12, 0]
-    and parsecard("Qd") == [10, 1]
+    str(
+        parsecard("7h") == [5, 2]
+        and parsecard("Ac") == [12, 0]
+        and parsecard("Qd") == [10, 1]
+    )
+    + " : function parsecard in hand.py"
 )
 
 # test if Hand class method cards is giving desired outputs
 print(
-    Hand("7hAcQd9c9d").cards == [[5, 2], [12, 0], [10, 1], [7, 0], [7, 1]]
-    and Hand("7dAdQd9c9d").cards == [[5, 1], [12, 1], [10, 1], [7, 0], [7, 1]]
-    and Hand("7h8cJdTc9d").cards == [[5, 2], [6, 0], [9, 1], [8, 0], [7, 1]]
+    str(
+        Hand("7hAcQd9c9d").cards == [[5, 2], [12, 0], [10, 1], [7, 0], [7, 1]]
+        and Hand("7dAdQd9c9d").cards == [[5, 1], [12, 1], [10, 1], [7, 0], [7, 1]]
+        and Hand("7h8cJdTc9d").cards == [[5, 2], [6, 0], [9, 1], [8, 0], [7, 1]]
+    )
+    + " : method cards in Hand class in hand.py"
 )
 
 
