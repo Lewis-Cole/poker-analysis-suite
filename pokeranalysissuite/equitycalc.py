6df92b0b58523ea7335b124b1cd9e33fdbcda9d2
Lewis Cole <lcole.mail@gmail.com>
2022-12-26 13:58:00 +0000

    Create file equitycalc.py and design structure

---
+++ pokeranalysissuite/equitycalc.py
@@ -0,0 +1,28 @@
+# - File: equitycalc.py
+#   - function calculateequity() input: ranges/hands, output: equity (hand by hand)
+
+# template function
+def calculateequity() -> None:
+    """
+    Summary line.
+
+    Extended description of function.
+
+    >>> templatefunction(examplearguments)
+    exampleoutput
+
+    Arguments
+    ---------
+    arg1 : int
+        Description of arg1
+    arg2 : str
+        Description of arg2
+
+    Returns
+    -------
+    int
+        Description of return value
+
+    """
+
+    return
