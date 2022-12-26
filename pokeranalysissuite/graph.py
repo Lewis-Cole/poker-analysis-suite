bcc4673b071fe943911059033f4206569f502e52
Lewis Cole <lcole.mail@gmail.com>
2022-12-26 13:58:20 +0000

    Create file graph.py and design structure

---
+++ pokeranalysissuite/graph.py
@@ -0,0 +1,28 @@
+# - File: graph.py
+#   - produces desired graph
+
+# template function
+def makegraph() -> None:
+    """
+    Summary line.
+
+    Extended description of function.
+
+    >>> templatefunction(examplearguments)
+    exampleoutput
+
+    Arguments
+    ---------
+    arg1 : int
+        Description of arg1
+    arg2 : str
+        Description of arg2
+
+    Returns
+    -------
+    int
+        Description of return value
+
+    """
+
+    return
