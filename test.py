89bb278526e72edaa808c7fe0227fc9c4a1eef48
Lewis Cole <lcole.mail@gmail.com>
2022-12-27 10:59:57 +0000

    Add test for teststraight function in hand class

--- test.py
+++ test.py
@@ -15,13 +15,29 @@ print(
 )
 
 # test if Hand class method cards is giving desired outputs
+h1 = Hand("7hAcQd9c9d")
+h2 = Hand("7d8dQdTd9d9cJd")
+h3 = Hand("7h8cJdTc9dAh2s")
+h4 = Hand("7h4c3dTc5dAh2s")
+
 print(
     str(
-        Hand("7hAcQd9c9d").cards == [[5, 2], [12, 0], [10, 1], [7, 0], [7, 1]]
-        and Hand("7dAdQd9c9d").cards == [[5, 1], [12, 1], [10, 1], [7, 0], [7, 1]]
-        and Hand("7h8cJdTc9d").cards == [[5, 2], [6, 0], [9, 1], [8, 0], [7, 1]]
+        h1.cards == [[5, 2], [12, 0], [10, 1], [7, 0], [7, 1]]
+        and h2.cards == [[5, 1], [6, 1], [10, 1], [8, 1], [7, 1], [7, 0], [9, 1]]
+        and h3.cards == [[5, 2], [6, 0], [9, 1], [8, 0], [7, 1], [12, 2], [0, 3]]
+        and h4.cards == [[5, 2], [2, 0], [1, 1], [8, 0], [3, 1], [12, 2], [0, 3]]
     )
     + " : method cards in Hand class in hand.py"
+)
+
+print(
+    str(
+        h1.teststraight(h1.rankcount) == (False, None)
+        and h2.teststraight(h2.rankcount) == (True, 10)
+        and h3.teststraight(h3.rankcount) == (True, 9)
+        and h4.teststraight(h4.rankcount) == (True, 3)
+    )
+    + " : function teststraight in Hand class in hand.py"
 )
 
 
