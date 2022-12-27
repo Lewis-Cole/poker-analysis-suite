42dd34b14b70a880081efe5ad38523aba460d9d2
Lewis Cole <lcole.mail@gmail.com>
2022-12-27 13:51:36 +0000

    Modify test print functions

--- test.py
+++ test.py
@@ -6,12 +6,10 @@ from pokeranalysissuite.hand import parsecard, Hand
 
 # test if function parsecard in hand.py is giving desired outputs
 print(
-    str(
-        parsecard("7h") == [5, 2]
-        and parsecard("Ac") == [12, 0]
-        and parsecard("Qd") == [10, 1]
-    )
-    + " : function parsecard in hand.py"
+    parsecard("7h") == [5, 2]
+    and parsecard("Ac") == [12, 0]
+    and parsecard("Qd") == [10, 1],
+    ": function parsecard in hand.py",
 )
 
 # test if Hand class method cards is giving desired outputs
@@ -21,23 +19,19 @@ h3 = Hand("7h8cJdTc9dAh2s")
 h4 = Hand("7h4c3dTc5dAh2s")
 
 print(
-    str(
-        h1.cards == [[5, 2], [12, 0], [10, 1], [7, 0], [7, 1]]
-        and h2.cards == [[5, 1], [6, 1], [10, 1], [8, 1], [7, 1], [7, 0], [9, 1]]
-        and h3.cards == [[5, 2], [6, 0], [9, 1], [8, 0], [7, 1], [12, 2], [0, 3]]
-        and h4.cards == [[5, 2], [2, 0], [1, 1], [8, 0], [3, 1], [12, 2], [0, 3]]
-    )
-    + " : method cards in Hand class in hand.py"
+    h1.cards == [[5, 2], [12, 0], [10, 1], [7, 0], [7, 1]]
+    and h2.cards == [[5, 1], [6, 1], [10, 1], [8, 1], [7, 1], [7, 0], [9, 1]]
+    and h3.cards == [[5, 2], [6, 0], [9, 1], [8, 0], [7, 1], [12, 2], [0, 3]]
+    and h4.cards == [[5, 2], [2, 0], [1, 1], [8, 0], [3, 1], [12, 2], [0, 3]],
+    ": method cards in Hand class in hand.py",
 )
 
 print(
-    str(
-        h1.teststraight(h1.rankcount) == (False, None)
-        and h2.teststraight(h2.rankcount) == (True, 10)
-        and h3.teststraight(h3.rankcount) == (True, 9)
-        and h4.teststraight(h4.rankcount) == (True, 3)
-    )
-    + " : function teststraight in Hand class in hand.py"
+    h1.teststraight(h1.rankcount) == (False, None)
+    and h2.teststraight(h2.rankcount) == (True, 10)
+    and h3.teststraight(h3.rankcount) == (True, 9)
+    and h4.teststraight(h4.rankcount) == (True, 3),
+    ": function teststraight in Hand class in hand.py",
 )
 
 
